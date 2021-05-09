9f5c81a839aabb8a7003cc3fe9d1d3bfd49d4e20
Wilson Leung <wilson.leung@gravitysuppychain.com>
2021-05-09 22:05:12 +0800

    fix bug

--- BankingSystem/src/main/java/banking/Bank.java
+++ BankingSystem/src/main/java/banking/Bank.java
@@ -9,10 +9,16 @@ import java.util.UUID;
  */
 public class Bank implements BankInterface {
 	private LinkedHashMap<Long, Account> accounts;
+	private Long sequentialAccNum;
+
+	public long getNextSeq() {
+		return sequentialAccNum++;
+	}
 
 	public Bank() {
 		// complete the function
 		accounts = new LinkedHashMap<Long, Account>();
+		sequentialAccNum = 0L;
 	}
 
 	private Account getAccount(Long accountNumber) {
@@ -22,8 +28,7 @@ public class Bank implements BankInterface {
 
 	public Long openCommercialAccount(Company company, int pin, double startingDeposit) {
 		// complete the function
-		String uk = company.getCompanyName()+company.getIdNumber();
-		Long accountNumber = UUID.nameUUIDFromBytes(uk.getBytes()).getMostSignificantBits();
+		Long accountNumber = getNextSeq();
 		CommercialAccount commercialAccount = new CommercialAccount(company, accountNumber, pin, startingDeposit);
 		accounts.put(accountNumber, commercialAccount);
 		return accountNumber;
@@ -31,8 +36,7 @@ public class Bank implements BankInterface {
 
 	public Long openConsumerAccount(Person person, int pin, double startingDeposit) {
 		// complete the function
-		String uk = person.getFirstName()+person.getLastName()+person.getIdNumber();
-		Long accountNumber = UUID.nameUUIDFromBytes(uk.getBytes()).getMostSignificantBits();
+		Long accountNumber = getNextSeq();
 		ConsumerAccount consumerAccount = new ConsumerAccount(person, accountNumber, pin, startingDeposit);
 		accounts.put(accountNumber, consumerAccount);
         return accountNumber;
